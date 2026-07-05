bad52c2fd604e42af5f1ed97248da4e6218e3f99
JerryGuo2001 <jianleg@uci.edu>
2026-07-04 21:09:50 -0700

    debug

--- static/js/game_env.js
+++ static/js/game_env.js
@@ -4,7 +4,7 @@
 let character, gravity, keys, currentCanvas, showPrompt, currentQuestion, totalMushrooms, collectedMushrooms, atRightEdge, change_detect_right, change_detect_left,totalQuestions,totalQuestions_setup;
 let exploration_debug=false
 // turn it off when you don't want practice
-let practice_explore_on_off=on
+let practice_explore_on_off= true
 if (practice_explore_on_off){
   totalQuestions=1
 }
